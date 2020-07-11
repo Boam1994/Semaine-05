0b64483bb0a7bdd84440f7fe94dfa7c7b7e4d36a
Boam <boam.tchegli30@gmail.com>
2020-07-12 00:00:00 +0200

    landing page exercice

--- Exercices/exercices-javascript/exercices-complementaires/script.js
+++ Exercices/exercices-javascript/exercices-complementaires/script.js
@@ -1,11 +1,31 @@
 // 1. Définissez une nouvelle variable "myHeaders", contenant un objet global Headers, configuré avec la paire de clé/valeur suivante: "Content-Type": "application/json"
-
+let myHeaders = new Headers({"Content-Type": "application/json"})
 // 2. Créez un formulaire dans votre index.html. Utilisez les balises "form", "label", "input", "button", passez comme ID "my-form" à votre balise form, ensuite les attributs pour les attributs "name" de vos input il est OBLIGATOIRE d'utiliser les clés attendues par l'API. C'est à dire une clé "auteur" et une clé "comment". Un de vos deux input aura donc comme valeur à l'attribut "name": "auteur", et le second "name":"comment". Pour finir, donnez comme ID "submit-btn" à votre bouton.
 
 // 3. Maintenant que vous avez construit votre formulaire: nous allons créer une variable nommée "form" qui pointe directement vers votre formulaire grâce à la méthode getElementById
 
 //4. Vous disposez de vos headers, et d'une variable pointant vers votre formulaire. Maintenant vous allez créer une écoute d'évenement sur le bouton ayant pour id "submit-btn". 
+    document.getElementById('submit').addEventListener("click", () => {
+        let authorValue = document.getElementById('auteur').Value;
+        let commentValue = document.getElementById('comment').Value;
+        let body = {
+            "auteur": authorValue,
+            "comment": commentValue,
+        }
+        console.log(JSON.stringify(body))
 
+    })
+    fetch("https://quotes-light-api.herokuapp.com/api/comments/", {method: "GET"})
+    .then(response => {
+        console.log(response);  
+      return response.json();
+      
+    }).then(response => {
+        console.log(response);
+    })
+    //console.log(response);
+
+    
 //5. A l'intérieur de cette écoute, vous allez créer une variable "myform" contenant un nouvel objet FormData qui pointe vers votre formulaire, utilisez la variable "form" créée précédement.
 
 //6. Maintenant que nous possédons tout le nécessaire à la rédaction de la méthode fetch(), lançons nous! Créez une méthode fetch qui utilise cette url : https://quotes-light-api.herokuapp.com/api/comments/ 
